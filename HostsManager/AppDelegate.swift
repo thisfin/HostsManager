20bcadccd19439f09c01fcb155e3aef2cfa8d817
文优 <thisfin@gmail.com>
2016-12-23 16:25:16 +0800

    New project

---
+++ HostsManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HostsManager
+//
+//  Created by wenyou on 2016/12/23.
+//  Copyright © 2016年 wenyou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HostsManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HostsManager
+//
+//  Created by wenyou on 2016/12/23.
+//  Copyright © 2016年 wenyou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
