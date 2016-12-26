46e9c5935e1e0ce0ba8bdd1889ee5776540b5577
文优 <thisfin@gmail.com>
2016-12-26 15:57:16 +0800

    状态栏

--- HostsManager/AppDelegate.swift
+++ HostsManager/AppDelegate.swift
@@ -11,8 +11,45 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
+    var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+//        Application.shared().mainMenu = statusMenu
+
+        statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        statusItem.title = ""
+        statusItem.image = {
+            let image = NSImage(named: "icon.png")
+            image?.size = NSSize(width: 20, height: 20)
+            return image
+        }()
+        let statusMenu = NSMenu()
+        statusItem.menu = statusMenu
+
+        let quitMenuItem = NSMenuItem()
+        quitMenuItem.title = "quitt"
+        quitMenuItem.action = #selector(AppDelegate.quitClicked(_:))
+        statusMenu.addItem(quitMenuItem)
+
+
+
+
+
+
+//        let statusItem: NSStatusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+//        statusItem.title = "hello"
+//        statusItem.highlightMode = true
+//
+//        if let button = statusItem.button {
+//            button.title = "hello"
+////            button.image = NSImage(named: "StatusBarButtonImage")
+////            button.action = Selector("printQuote:")
+//        }
+
+
+
+
+
         window = NSWindow(contentRect: NSRect(x: 0, y:0, width: 400, height: 400),
                           styleMask: [.closable, .resizable, .miniaturizable, .titled],
                           backing: .buffered,
@@ -28,6 +65,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
 }
-
