d64740cf25056adc8f5d59f31c6e153ab7cfd5c5
文优 <thisfin@gmail.com>
2017-07-01 19:55:37 +0800

    udpate

--- HostsManager/AlertPanel.swift
+++ HostsManager/AlertPanel.swift
@@ -13,7 +13,7 @@ class AlertPanel  {
     class func show(_ content: String, fontSize: CGFloat = 32) {
         let font = NSFont.systemFont(ofSize: fontSize)
         let size = (content as NSString).size(withAttributes: [NSFontAttributeName: font])
-        let panel = NSPanel.init(contentRect: NSMakeRect(0, 0, size.width + 100, size.height + 40), styleMask: [.borderless, .hudWindow], backing: .buffered, defer: true)
+        let panel = NSPanel(contentRect: NSMakeRect(0, 0, size.width + 100, size.height + 40), styleMask: [.borderless, .hudWindow], backing: .buffered, defer: true)
         let textField = NSTextField.init(frame: panel.contentLayoutRect)
         textField.cell = WYVerticalCenterTextFieldCell()
         textField.isEditable = false
@@ -23,16 +23,16 @@ class AlertPanel  {
         textField.textColor = .white
         textField.font = font
         textField.stringValue = content
-        if let contentView = panel.contentView {
+        if let contentView = panel.contentView { // 此处做颜色和圆角的处理
             contentView.addSubview(textField)
             contentView.wantsLayer = true
-            contentView.layer?.backgroundColor = panel.backgroundColor.cgColor
             contentView.layer?.cornerRadius = 5
+            contentView.layer?.backgroundColor = panel.backgroundColor.cgColor // 使用默认 hudWindow 的颜色
+            panel.backgroundColor = .clear
         }
-        panel.backgroundColor = .clear
         panel.center()
         panel.orderFront(self)
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.8, execute: {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.8, execute: { // 自动关闭
             panel.orderOut(self)
             panel.close()
         })
